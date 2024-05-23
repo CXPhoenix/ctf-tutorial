891ebddb1063fa6d39b0f14decc228d51875bb95
CXPhoenix <jate9229@gmail.com>
2024-05-24 02:03:11 +0800

    :sparkles: 完成一部分的權限控制初探內容

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./.vitepress/theme/components/**/*.{vue,js,jsx}"],
+  content: [
+    "./.vitepress/theme/components/**/*.{vue,js,jsx}",
+    "**/*.{md, html}",
+  ],
   theme: {
     extend: {},
   },
